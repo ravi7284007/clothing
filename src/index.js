e68a1dae7c300f5eb43267ceae9145dc4b4e25b4
Ravi Kumar <ravi7284007@gmail.com>
2022-11-12 08:45:12 +0530

    redux persist for remembering data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 // import { CategoriesProvider } from './context/categories.context';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
@@ -15,9 +15,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,27 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { cartReducer } from './cart/cart.reducer';
 
 import { rootReducer } from './root-reducer';
 
 const middleWares = [logger];
 
+const persistConfig = {
+    key: 'rrot',
+    storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
 const composedEnhancers = compose(applyMiddleware(...middleWares))
 
 export const store = createStore(
-    rootReducer,
+    persistedReducer,
     undefined,
     composedEnhancers);
+
+export const persistor = persistStore(store)
